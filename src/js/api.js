bc8361e22b238271fa00680dbda8cee9d7cd1684
NkAndre <andre270708@gmail.com>
2026-05-30 16:50:37 -0300

    url da api add

--- src/js/api.js
+++ src/js/api.js
@@ -1,1 +1,10 @@
 //consumos de dados(fetch)
+
+export async function buscarCotação(moedaOrigem,moedaDestino) {
+    const link =`https://economia.awesomeapi.com.br/json/last/${moedaOrigem}-${moedaDestino}`;
+
+    const resposta = await fetch (link);
+    const dados = await resposta.json();
+
+    return dados[`${moedaOrigem}${moedaDestino}`].bid;
+}
